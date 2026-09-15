21ecab064c784efcb76b318fd2ccdbb1e7676d49
aviralumrao <aviral0419@gmail.com>
2026-09-16 00:23:44 +0530

    Initial commit

---
+++ src/test/java/com/example/Notes/App/NotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Notes.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
