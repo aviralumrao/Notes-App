f3d2c05a88e753d3c6026b56b3d547df63837463
aviralumrao <aviral0419@gmail.com>
2026-09-17 21:05:21 +0530

    add :  folder named config to kept checkstyle configs

--- src/main/java/com/example/Notes/App/repository/NoteRepository.java
+++ src/main/java/com/example/Notes/App/repository/NoteRepository.java
@@ -4,5 +4,6 @@ import com.example.Notes.App.model.Note;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoteRepository extends JpaRepository<Note, UUID> {
+public interface
+NoteRepository extends JpaRepository<Note, UUID> {
 }
