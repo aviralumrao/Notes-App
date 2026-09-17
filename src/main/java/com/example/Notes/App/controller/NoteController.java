e360b35e467a3b3df40bb26bf7997e40ca102b77
aviralumrao <aviral0419@gmail.com>
2026-09-17 08:24:12 +0530

    fix :Add checkstyle configuration

--- src/main/java/com/example/Notes/App/controller/NoteController.java
+++ src/main/java/com/example/Notes/App/controller/NoteController.java
@@ -2,9 +2,15 @@ package com.example.Notes.App.controller;
 
 import com.example.Notes.App.model.Note;
 import com.example.Notes.App.repository.NoteRepository;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -46,7 +52,4 @@ public class NoteController {
     public void deleteNote(@PathVariable Long id) {
         noteRepository.deleteById(id);
     }
-
-
 }
-
--- src/main/java/com/example/Notes/App/model/Note.java
+++ src/main/java/com/example/Notes/App/model/Note.java
@@ -1,9 +1,7 @@
 package com.example.Notes.App.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
@@ -19,29 +17,32 @@ public class Note {
     public Note() {
     }
 
-    public Note(String title,String content){
+    public Note(String title, String content) {
         this.title = title;
         this.content = content;
     }
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
-    public void setId(Long id){
+
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
-    public void setTitle(String title){
+
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getContent(){
+    public String getContent() {
         return content;
     }
-    public void setContent(String content){
+
+    public void setContent(String content) {
         this.content = content;
     }
 }
