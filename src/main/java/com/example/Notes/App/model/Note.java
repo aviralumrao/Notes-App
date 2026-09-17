c9e0eaeb452a6e80b5704ce34a2303bc0c1eff1a
aviralumrao <aviral0419@gmail.com>
2026-09-17 13:40:02 +0530

    fix: used getter and setter

--- src/main/java/com/example/Notes/App/model/Note.java
+++ src/main/java/com/example/Notes/App/model/Note.java
@@ -5,7 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Note {
 
@@ -15,36 +23,4 @@ public class Note {
 
     private String title;
     private String content;
-
-    public Note() {
-    }
-
-    public Note(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
